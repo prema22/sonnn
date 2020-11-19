2d83f467644e70816a6341cf128dd95412fe45d5
prema22 <71822783+prema22@users.noreply.github.com>
2020-11-19 11:26:03 +0530

    tgy
    gh

--- src/sunp2/sunc2.java
+++ src/sunp2/sunc2.java
@@ -5,11 +5,8 @@ public class sunc2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("fhytttt");
-System.out.println("pppppppppp");
-		System.out.println("ssssssssssss");
-		
 
-		System.out.println("aruyuuuuuuuuuuu");
+		
 	}
 
 }
